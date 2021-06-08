a6f468d017c39e62c654386e09b2689101aeb247
adillashidiq <adill.ashidiq20@gmail.com>
2021-06-08 15:06:18 +0700

    menambahkan fungsi input

--- app/Controllers/Tugas.php
+++ app/Controllers/Tugas.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\TugasModel;
+use CodeIgniter\HTTP\Request;
 
 class Tugas extends BaseController
 
@@ -34,7 +35,6 @@ class Tugas extends BaseController
     return view('tugas/add-tugas', $data);
   }
 
-
   public function detail($slug)
   {
     $data = [
@@ -42,6 +42,25 @@ class Tugas extends BaseController
       'tugas' => $this->tugasModel->getTugas($slug)
     ];
 
+    if (empty($data['tugas'])) {
+      throw new \CodeIgniter\Exceptions\PageNotFoundException('Nama tugas ' . $slug . ' tidak ditemukan');
+    }
+
     return view('tugas/detail', $data);
+  }
+
+  public function input()
+  {
+    $slug = url_title($this->request->getVar('nama_surah'), '-', true);
+
+    $this->tugasModel->save([
+      'nama_surah' => $this->request->getVar('nama_surah'),
+      'slug' => $slug,
+      'tugas' => $this->request->getVar('tugas'),
+    ]);
+
+    session()->setFlashdata('pesan', 'Berhasil menambahkan tugas baru');
+
+    return redirect()->to('/tugas');
   }
 }
