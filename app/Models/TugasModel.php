76e7d065e788b53adeb770e2d58ca99354b18a89
adillashidiq <adill.ashidiq20@gmail.com>
2021-06-08 12:28:10 +0700

    menambah fungsi getTugas (slug)

--- app/Models/TugasModel.php
+++ app/Models/TugasModel.php
@@ -7,4 +7,14 @@ use CodeIgniter\Model;
 class TugasModel extends Model
 {
   protected $table = 'tugas';
+
+
+  public function getTugas($slug = false)
+  {
+    if ($slug == false) {
+      return $this->findAll();
+    }
+
+    return $this->where(['slug' => $slug])->first();
+  }
 }
