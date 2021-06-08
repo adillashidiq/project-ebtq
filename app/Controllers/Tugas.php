8aabfbf866dc0375c726931646109b1ffc5f9cc4
adillashidiq <adill.ashidiq20@gmail.com>
2021-06-08 12:28:56 +0700

    menambahkan fungsi detail dan merubah fungsi index

--- app/Controllers/Tugas.php
+++ app/Controllers/Tugas.php
@@ -18,10 +18,10 @@ class Tugas extends BaseController
 
   public function index()
   {
-    $tugas = $this->tugasModel->findAll();
+    // $tugas = $this->tugasModel->findAll();
     $data = [
       'title' => 'Tugas | E-BTQ HMJ TI',
-      'tugas' => $tugas
+      'tugas' => $this->tugasModel->getTugas()
     ];
     return view('tugas/index', $data);
   }
@@ -37,5 +37,11 @@ class Tugas extends BaseController
 
   public function detail($slug)
   {
+    $data = [
+      'title' => 'Detail Tugas | E-BTQ HMJ TI',
+      'tugas' => $this->tugasModel->getTugas($slug)
+    ];
+
+    return view('tugas/detail', $data);
   }
 }
